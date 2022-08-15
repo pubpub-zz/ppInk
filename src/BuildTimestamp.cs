00d838192948d2b42229baa86e6396afbf43af21
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2022-08-15 10:05:15 +0200

    REL : v1.8.3

--- src/BuildTimestamp.cs
+++ src/BuildTimestamp.cs
@@ -1,1 +1,1 @@
-namespace gInk { public static class Build { public static string Timestamp = "28/08/2021 13:41:08.67".Substring(0,16);}} 
+namespace gInk { public static class Build { public static string Timestamp = "30/07/2022 22:48:05.13".Substring(0,16);}} 

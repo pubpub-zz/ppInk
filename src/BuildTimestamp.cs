2ca700bbf3320d08f946509be20edc98cb1b659f
pubpub-zz <4083478+pubpub-zz@users.noreply.github.com>
2024-04-30 16:05:29 +0200

    Late commit

--- src/BuildTimestamp.cs
+++ src/BuildTimestamp.cs
@@ -1,1 +1,1 @@
-namespace gInk { public static class Build { public static string Timestamp = "30/04/2024  9:23:24.11".Substring(0,16);}} 
+namespace gInk { public static class Build { public static string Timestamp = "30/04/2024 11:16:30.61".Substring(0,16);}} 
